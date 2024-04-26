fb3529e3e61c55c82140194c500fa3e48fb6d195
Dash Skndash <dashskndash@gmail.com>
2024-04-26 17:34:53 +0530

    init

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-const PORT = 3000;
-
-app.use("/", express.static("build/assets"));
-app.use("/", express.static("build"));
-
-app.listen(PORT, () => console.log("Listening on port "+PORT));
